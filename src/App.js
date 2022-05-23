c2635cb7d6f5d9690af3ea14d60026171b6a27cd
Sulav <sulavshrestha023@gmail.com>
2022-05-23 11:11:35 +0545

    imported react-icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+import {FaQuoteRight} from 'react-icons/fa';
+import data from 'data';
+import { useState } from 'react';
 
 function App() {
+  const[people, setPeople] = useState(data);
+  const[index, setIndex] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
