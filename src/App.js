19acd8b75761015ff73191b71385c332e0a06e36
Sulav <sulavshrestha023@gmail.com>
2022-05-23 11:13:15 +0545

    imported css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
-import {FaQuoteRight} from 'react-icons/fa';
-import data from 'data';
-import { useState } from 'react';
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+import { useState } from "react";
 
 function App() {
-  const[people, setPeople] = useState(data);
-  const[index, setIndex] = useState(0);
-  return (
-    
-  );
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  return <section className="section"></section>;
 }
 
 export default App;
